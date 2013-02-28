a2971bbca1414c34a0d88d3b922c8c948a18391d
Alexander Pope <alex@pope-industries.com>
2013-02-28 15:46:34 +0100

    added handlebars template compiler

---
+++ test/fixtures/builder/build/project/buddy_single-handlebars-file.js
@@ -0,0 +1,11 @@
+exports.build = {
+	"js": {
+		"sources": ["src/handlebars"],
+		"targets": [
+			{
+				"input": "src/handlebars/template.handlebars",
+				"output": "output"
+			}
+		]
+	}
+}
--- test/fixtures/builder/build/project/output/main.js
+++
@@ -1,4 +0,0 @@
-/*BUILT Thu Feb 28 2013 14:42:34 GMT+0100 (CET)*/
-require.register('main', function(module, exports, require) {
-  
-});
