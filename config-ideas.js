a66cdb118a53ce507204ec4baecc6bca4f059e4f
Alexander Pope <alex@pope-industries.com>
2015-05-18 15:00:17 +0200

    add config ideas

---
+++ config-ideas.js
@@ -0,0 +1,144 @@
+{
+	build: {
+		sources: ['src'],
+		targets: [
+			{
+				input: 'src/main.js',
+				output: 'www',
+				bootstrap: true,
+				boilerplate: true
+			},
+			{
+				input: 'src/main.css',
+				output: 'www'
+			}
+		],
+		after: '',
+		afterEach: '',
+		server: {
+			port: 3000,
+			env: {
+				"FOO": 'foo'
+			}
+		}
+	}
+}
+
+{
+	build: {
+		targets: [
+			{
+				input: ['src/main.js', 'src/main.css'],
+				output: 'www',
+				bootstrap: true,
+				boilerplate: true
+			}
+		]
+	}
+}
+
+{
+	build: {
+		targets: [
+			{
+				input: 'src/libs.js',
+				output: 'www',
+				bootstrap: true,
+				boilerplate: true,
+				targets: [
+					{
+						// parse manifest
+						input: 'node_modules/app',
+						//
+						// use manifest 'name' as filename
+						output: 'www',
+						targets: [
+							{
+								input: ['node_modules/gateway', 'node_modules/extreme', 'node_modules/search'],
+								output: 'www'
+							}
+						]
+					}
+				]
+			},
+			{
+				// parse dep tree
+				input: 'index.js',
+				server: true
+			}
+		]
+	}
+}
+
+// Build subprojects
+{
+	build: {
+		targets: [
+			'node_modules/app',
+			'node_modules/gateway',
+			'node_modules/extreme',
+			'node_modules/search'
+		]
+	}
+}
+
+{
+	build: {
+		targets: [
+			{
+				input: 'node_modules/app:libs.js+node_modules/gateway:libs.js+node_modules/extreme:libs.js',
+				output: 'www/libs.js'
+				bootstrap: true,
+				boilerplate: true,
+				targets: [
+					{
+						input: 'node_modules/app:client.js',
+						output: 'www/index.js',
+						targets: [
+							{
+								input: ['node_modules/gateway:client.js', 'node_modules/extreme:client.js', 'node_modules/search:client.js'],
+								output: 'www'
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+}
+
+{
+	build: {
+		targets: [
+			{
+				input: [
+					// shared deps
+					'node_modules/{app,gateway,extreme,search}/libs.js',
+					'node_modules/{app,gateway,extreme,search}/client.js'
+				],
+				output: [
+					// concat
+					'www/libs.js',
+					// unique names?
+					'www'
+				],
+				// first output
+				bootstrap: true,
+				boilerplate: true
+			}
+		]
+	}
+}
+
+{
+	build: {
+		targets: [
+			{
+				cmd: [
+					'node_modules/{app,gateway,extreme,search}/libs.js>www/libs.js',
+					'node_modules/{app,gateway,extreme,search}/client.js>www'
+				]
+			}
+		]
+	}
+}
