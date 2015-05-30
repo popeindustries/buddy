a56ac75e9f108b258e6ebbaf26a6c71ea7f8a6f9
Alexander Pope <alex@pope-industries.com>
2015-05-30 20:18:12 +0200

    begin builder refactor

---
+++ test/5-builder-test.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var path = require('path')
+	, fs = require('fs')
+	, rimraf = require('rimraf')
+	, should = require('should')
+	, Builder = require('../lib/builder')
+	, config = require('../lib/config');
+
+function gatherFiles (dir, files) {
+	files = files || [];
+	for (var item in fs.readdirSync(dir)) {
+		var p = path.resolve(dir, item);
+		if (fs.statSync(p).isFile()) {
+			files.push(p);
+		} else {
+			gatherFiles(p, files);
+		}
+	}
+	return files;
+}
+
+describe.skip('Builder', function () {
+	before(function () {
+		process.chdir(path.resolve(__dirname, 'fixtures/builder'));
+	});
+	beforeEach(function () {
+		this.builder = new Builder();
+	});
+	afterEach(function () {
+		this.builder = null;
+		rimraf.sync(path.resolve('output'));
+	});
+
+	describe('init', function () {
+		before(function () {
+			process.chdir(path.resolve(__dirname, 'fixtures/builder/init'));
+		});
+		it('should initialize a single target', function () {
+			var targets = this.builder.initTargets([{
+				inputPath: path.resolve('target/foo.js'),
+				input: 'target/foo.js',
+				output: 'main.js',
+				runtimeOptions: {}
+			}]);
+			targets.should.have.length(1);
+		});
+		it('should initialize a single target with nested child target', function () {
+			var targets = this.builder.initTargets([{
+				inputPath: path.resolve('target/foo.js'),
+				input: 'target/foo.js',
+				output: 'main.js',
+				hasChildren: true,
+				runtimeOptions: {},
+				targets:[{
+					inputPath: path.resolve('target/lib'),
+					input:'target/lib',
+					output:'../js',
+					runtimeOptions: {}
+				}]
+			}]);
+			targets.should.have.length(1);
+			targets[0].targets.should.have.length(1);
+		});
+	});
+
+	describe('build', function () {
+		before(function () {
+			process.chdir(path.resolve(__dirname, 'fixtures/builder/build/simple'));
+		});
+		describe('simple file targets', function () {
+			it('should build 1 js file when passed a JSON config object', function (done) {
+				this.builder.build({
+					build: {
+						"targets": [
+							{
+								"input": "foo.js",
+								"output": "output"
+							}
+						]
+					}
+				}, null, function (err, filepaths) {
+					fs.existsSync(filepaths[0]).should.be.true;
+					done();
+				});
+			});
+		});
+
+		describe('directory targets', function () {
+
+		});
+
+		describe('project targets', function () {
+
+		});
+	});
+
+});
