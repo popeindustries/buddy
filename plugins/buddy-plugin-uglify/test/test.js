7644f5d6ee0aad8dd54abeac0d2bc8e3ff8d01e4
Alexander Pope <alex@pope-industries.com>
2016-01-12 09:50:34 +0100

    add uglify plugin

---
+++ plugins/buddy-plugin-uglify/test/test.js
@@ -0,0 +1,14 @@
+const expect = require('expect.js')
+  , compress = require('..').compress
+  , path = require('path')
+  , fs = require('fs');
+
+describe('buddy-plugin-uglify', () => {
+  before(() => {
+    process.chdir('./test/fixtures');
+  });
+
+  it('should', (done) => {
+    done();
+  });
+});
