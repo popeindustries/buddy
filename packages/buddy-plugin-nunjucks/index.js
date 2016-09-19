0fbf540c0c18755918525273ece7ba708534f22b
Alexander Pope <alex@pope-industries.com>
2016-09-19 20:34:57 +0200

    begin nunjucks plugin

--- packages/buddy-plugin-nunjucks/index.js
+++ packages/buddy-plugin-nunjucks/index.js
@@ -2,53 +2,159 @@
 
 const nunjucks = require('nunjucks');
 
-const Template = nunjucks.Template;
+const FILE_EXTENSIONS = ['nunjucks', 'nunjs', 'njk'];
+const RE_INCLUDE = /{%\s(?:extends|include)\s['"]([^'"]+)['"]\s?%}/g;
+const WORKFLOW_WRITE = [
+  'inline',
+  'compile',
+  'parseInline',
+  'inlineInline'
+];
 
-let env;
-
-/**
- * Retrieve registration data
- */
-exports.registration = {
+module.exports = {
   name: 'nunjucks',
-  extensions: {
-    html: [
-      'nunjucks',
-      'nunjs'
-    ]
+  type: 'html',
+
+  /**
+   * Register plugin
+   * @param {Config} config
+   */
+  register (config) {
+    config.registerFileDefinitionAndExtensionsForType(define, FILE_EXTENSIONS, this.type);
   }
 };
 
 /**
- * Compile 'content'
- * @param {String} content
- * @param {Object} options
- * @param {Function} fn(err, content)
+ * Extend 'File' with new behaviour
+ * @param {Class} File
+ * @param {Object} utils
+ * @returns {Class}
  */
-exports.compile = function (content, options, fn) {
-  // Setup cache
-  if (!env) {
-    env = new nunjucks.Environment(options.cache, { autoescape: true });
-    options.cache.lib = env;
-  }
+function define (File, utils) {
+  const { debug, strong } = utils.cnsl;
+  const { uniqueMatch } = utils.string;
 
-  try {
-    // Store includes
-    if (options.includes) {
-      options.includes.forEach((include) => {
-        if (!options.cache.hasSource(include.filepath)) {
-          options.cache.setSource(include.filepath, new Template(include.content, env, include.filepath));
-        }
+  return class NUNJUCKSFile extends File {
+    /**
+     * Constructor
+     * @param {String} id
+     * @param {String} filepath
+     * @param {Object} options
+     *  - {Object} fileExtensions
+     *  - {Function} fileFactory
+     *  - {Object} runtimeOptions
+     *  - {Array} sources
+     */
+    constructor (id, filepath, options) {
+      super(id, filepath, options);
+
+      this.workflows.write = WORKFLOW_WRITE;
+    }
+
+    /**
+     * Parse file contents for dependency references
+     * @param {Object} buildOptions
+     *  - {Boolean} bootstrap
+     *  - {Boolean} boilerplate
+     *  - {Boolean} bundle
+     *  - {Boolean} compress
+     *  - {Array} ignoredFiles
+     *  - {Boolean} includeHeader
+     *  - {Boolean} includeHelpers
+     *  - {Boolean} watchOnly
+     * @param {Function} fn(err)
+     */
+    parse (buildOptions, fn) {
+      // Add sidecar json file
+      const sidecarData = super.findSidecarDependency();
+      // Parse includes
+      let matches = uniqueMatch(this.content, RE_INCLUDE)
+        .map((match) => {
+          match.id = match.match;
+          return match;
+        });
+
+      if (sidecarData) matches.push(sidecarData);
+      super.addDependencies(matches, buildOptions);
+      fn();
+    }
+
+    /**
+     * Inline include dependency content
+     * @param {Object} buildOptions
+     *  - {Boolean} bootstrap
+     *  - {Boolean} boilerplate
+     *  - {Boolean} bundle
+     *  - {Boolean} compress
+     *  - {Array} ignoredFiles
+     *  - {Boolean} includeHeader
+     *  - {Boolean} includeHelpers
+     *  - {Boolean} watchOnly
+     * @param {Function} fn(err)
+     */
+    inline (buildOptions, fn) {
+      super.inlineDependencyReferences();
+      debug(`inline: ${strong(this.relpath)}`, 4);
+      fn();
+    }
+
+    /**
+     * Compile file contents
+     * @param {Object} buildOptions
+     *  - {Boolean} bootstrap
+     *  - {Boolean} boilerplate
+     *  - {Boolean} bundle
+     *  - {Boolean} compress
+     *  - {Array} ignoredFiles
+     *  - {Boolean} includeHeader
+     *  - {Boolean} includeHelpers
+     *  - {Boolean} watchOnly
+     * @param {Function} fn(err)
+     */
+    compile (buildOptions, fn) {
+      // TODO: find sidecar data file
+      const data = {};
+
+      nunjucks.renderString(this.content, data, (err, content) => {
+        if (err) return fn(err);
+        this.content = content;
+        debug(`compile: ${strong(this.relpath)}`, 4);
+        fn();
       });
     }
 
-    fn(null, env.renderString(content, options.data || {}, {
-      path: options.filepath,
-      name: options.filepath
-    }));
+    /**
+     * Parse file contents for inline dependency references
+     * @param {Object} buildOptions
+     *  - {Boolean} bootstrap
+     *  - {Boolean} boilerplate
+     *  - {Boolean} bundle
+     *  - {Boolean} compress
+     *  - {Array} ignoredFiles
+     *  - {Boolean} includeHeader
+     *  - {Boolean} includeHelpers
+     *  - {Boolean} watchOnly
+     * @param {Function} fn(err)
+     */
+    parseInline (buildOptions, fn) {
+      super.parse(buildOptions, fn);
+    }
 
-  } catch (err) {
-    err.filepath = options.filepath;
-    fn(err);
-  }
-};
+    /**
+     * Inline css/img/js dependency content
+     * @param {Object} buildOptions
+     *  - {Boolean} bootstrap
+     *  - {Boolean} boilerplate
+     *  - {Boolean} bundle
+     *  - {Boolean} compress
+     *  - {Array} ignoredFiles
+     *  - {Boolean} includeHeader
+     *  - {Boolean} includeHelpers
+     *  - {Boolean} watchOnly
+     * @param {Function} fn(err)
+     */
+    inlineInline (buildOptions, fn) {
+      super.inline(buildOptions, fn);
+    }
+  };
+}
