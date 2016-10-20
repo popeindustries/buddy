7830db211fe3478eaeb16548b4c919e591393b26
Alexander Pope <alex@pope-industries.com>
2016-10-20 12:52:22 +0200

    fix peerDependency; release buddy-plugin-nunjucks 2.5.2-3

--- packages/buddy-plugin-nunjucks/test/test.js
+++ packages/buddy-plugin-nunjucks/test/test.js
@@ -2,6 +2,7 @@
 
 const configFactory = require('../../../lib/config');
 const expect = require('expect.js');
+const fileCacheFactory = require('../../../lib/config/fileCache');
 const path = require('path');
 const plugin = require('../index');
 const fs = require('fs');
@@ -18,7 +19,7 @@ describe('buddy-plugin-nunjucks', () => {
     }, {});
     plugin.register(config);
     fileFactoryOptions = {
-      caches: config.caches,
+      fileCache: fileCacheFactory(),
       fileExtensions: config.fileExtensions,
       fileFactory: config.fileFactory,
       pluginOptions: { babel: { plugins: [] } },
