dcb55a1510a66d29073135b27ab36a64942147df
Alexander Pope <alex@pope-industries.com>
2016-11-20 17:47:17 +0100

    add stopwatch util

--- test/1-utils-test.js
+++ test/1-utils-test.js
@@ -5,6 +5,7 @@ const callable = require('../lib/utils/callable');
 const expect = require('expect.js');
 const filepath = require('../lib/utils/filepath');
 const path = require('path');
+const stopwatch = require('../lib/utils/stopwatch');
 
 describe('utils', () => {
   describe('callable', () => {
@@ -142,6 +143,40 @@ describe('utils', () => {
       it('should return the dir/file name of a file relative to current directory', () => {
         expect(filepath.filepathName('package.json')).to.equal('./package.json');
       });
+    });
+  });
+
+  describe('stopwatch', () => {
+    afterEach(() => {
+      stopwatch.clear('test');
+    });
+
+    it('should start and stop with elapsed duration', (done) => {
+      stopwatch.start('test');
+      setTimeout(() => {
+        const elapsed = stopwatch.stop('test');
+
+        expect(elapsed).to.be.within(500, 510);
+        done();
+      }, 500);
+    });
+    it('should start and stop with formatted duration in ms', (done) => {
+      stopwatch.start('test');
+      setTimeout(() => {
+        const elapsed = stopwatch.stop('test', true);
+
+        expect(elapsed).to.match(/^5[0-9]{2}ms$/);
+        done();
+      }, 500);
+    });
+    it('should start and stop with formatted duration in s', (done) => {
+      stopwatch.start('test');
+      setTimeout(() => {
+        const elapsed = stopwatch.stop('test', true);
+
+        expect(elapsed).to.match(/^1\.[0-9]s$/);
+        done();
+      }, 1100);
     });
   });
 });
