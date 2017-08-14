bbea4b248fbb9c5fdcb1c26be8c8c4ff398a4397
Alexander Pope <alex@pope-industries.com>
2017-08-14 17:16:38 +0200

    fix handling rename of function declarations

--- test/fixtures/file/src/namespace.js
+++ test/fixtures/file/src/namespace.js
@@ -1,3 +1,79 @@
+const bar = require('bar');
+const Bar = require('Bar');
+let foo = require('./foo');
+var boo;
 
-function e(g) {}
+console.log(foo, bar);
+
+foo = bar;
+foo && 'zoo';
+var baz = foo.baz;
+var boo = {};
+boo[baz] = 'baz';
+
+class Foo extends Bar {
+  constructor () {
+    super()
+    this.foo = foo;
+    console.log(foo);
+  }
+  bar () {
+    foo();
+  }
+}
+
+function bat (foo, options = {}) {
+  const f = new Foo();
+
+  console.log(f, foo, bar, 'bat', options);
+}
+
+for (let foo = 0; foo < 3; foo++) {
+  bat(foo);
+  console.log(bar);
+}
+
+zip = {
+  foo: foo
+};
+
+function y (...rest) {
+  console.log(rest)
+}
+
+var units = {};
+
+var z = function () {
+  var params = {
+    units: function(v) {
+      if (units[v]) {
+        var t = units[v].t;
+      }
+    }
+  };
+};
+
+var zing;
+
+if (true) {
+  zing = 'zing';
+}
+
+var { a, b } = require('c');
+
+function S () {
+  Object.assign(S.prototype, { foo: foo });
+
+  if (true) {
+    S = new Proxy(S, {});
+  }
+}
+
+if (true) {
+  var t = {};
+  let u;
+  t.foo = 'c';
+}
+
+function e(e) {}
 var f = e;
